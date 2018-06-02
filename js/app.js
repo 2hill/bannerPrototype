e285f7c17d1e004924e295c7dcc79c4955c1b40b
2hill <jamil.rahou@laposte.net>
2018-06-02 11:25:47 +0200

    modal inti

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// modal toggle
+
+let overlay = document.getElementById('overlay');
+
+function openModal() {
+    overlay.classList.remove("is-hidden");
+}
+
+function closeModal() {
+    overlay.classList.add("is-hidden");
+}
+
+
+
+
+// partie static: if opera div display none/ display grid  static Image.
