16c93ba848a605263db3104d7152bdf06a1bfb1b
2hill <jamil.rahou@laposte.net>
2018-06-02 12:31:11 +0200

    modal zindex

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@
 // modal toggle
 
 let overlay = document.getElementById('overlay');
+let open = document.getElementById('open').addEventListener("click", openModal);
+let close = document.getElementById('close').addEventListener("click", closeModal);
 
 function openModal() {
     overlay.classList.remove("is-hidden");
+
 }
 
 function closeModal() {
@@ -16,3 +19,5 @@ function closeModal() {
 
 
 // partie static: if opera div display none/ display grid  static Image.
+
+//when click on overlay or croix close de modal
