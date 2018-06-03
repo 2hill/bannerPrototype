79c246e3f42f872c02d984d665d6da4db4969394
2hill <jamil.rahou@laposte.net>
2018-06-03 15:50:12 +0200

    prefix and supports

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
 "use strict";
 
-// modal toggle
-
 let overlay = document.getElementById('overlay');
 let loadbg = document.getElementById('loadbg');
-
-
 let open = document.getElementById('open');
 let close = document.getElementById('close')
 
-
+// close the lightbox and prevent mouseover
 function closelightBox() {
-    overlay.style.display = "none"; 
+    overlay.style.display = "none";
+    open.removeEventListener("mouseover", load);
 }
-
+// open lightbox and remove loading animation after 3s
 function load() {
-     loadbg.style.display = "grid";
-     setTimeout(function(){ overlay.style.display = "grid";
-                            loadbg.style.display = "none";  }, 3000);
+    loadbg.style.display = "grid";
+    setTimeout(function () {
+        overlay.style.display = "grid";
+        loadbg.style.display = "none";
+    }, 3000);
 }
 
 open.addEventListener("mouseover", load);
 open.addEventListener("click", load);
 close.addEventListener("click", closelightBox);
-//overlay.addEventListener("click", closelightBox);
+
 
 
 //responsive
@@ -32,5 +31,5 @@ close.addEventListener("click", closelightBox);
 
 // reduire img size
 
-//prevent modal tant que animation pas finie*/
+//ajout compatiblite
 
